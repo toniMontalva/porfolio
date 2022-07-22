bf1111ecc2fb2682c6377e29afa8a9c8e63cdd5e
toniMontalva <tonimontalvapastor@gmail.com>
2022-07-23 01:24:43 +0200

    small data changes

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -10,27 +10,32 @@ export class SkillsComponent implements OnInit {
     {
       name: 'Angular - TypeScript',
       logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/240px-Angular_full_color_logo.svg.png',
-      expertise: '80%'
+      expertise: '80%',
+      description: 'Uso de rutas, rxjs operators, permisos con AuthGuard.'
     },
     {
       name: 'Ionic',
       logo: 'https://ionicframework.com/img/meta/logo.png',
-      expertise: '90%'
+      expertise: '90%',
+      description: 'Maquetación web y mobile responsive para ambas plataformas, utilización en variedad de proyectos.'
     },
     {
       name: 'dotNET - C#',
       logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/.NET_Core_Logo.svg/1200px-.NET_Core_Logo.svg.png',
-      expertise: '70%'
+      expertise: '70%',
+      description: 'WinForms, Entity Framework'
     },
     {
       name: 'CSS',
       logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/CSS3_logo.svg/800px-CSS3_logo.svg.png',
-      expertise: '90%'
+      expertise: '90%',
+      description: 'Diseño responsive, flexbox.'
     },
     {
       name: 'HTML',
       logo: 'https://www.w3.org/html/logo/downloads/HTML5_Badge_512.png',
-      expertise: '90%'
+      expertise: '90%',
+      description: 'Maquetación web.'
     },
   ];
   constructor() { }
