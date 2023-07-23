f1aa2957aa73a729308e296ea42d9fdff83ad547
toniMontalva <tonimontalvapastor@gmail.com>
2023-07-24 00:15:18 +0200

    Add experience

--- src/app/formacion/formacion.component.ts
+++ src/app/formacion/formacion.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-formacion',
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./formacion.component.scss'],
 })
 export class FormacionComponent implements OnInit {
+  @ViewChild('formacionInfo', { static: false}) pInfo: ElementRef;
   showFormacionInfo = false;
 
-  constructor() { }
+  constructor(private renderer2: Renderer2) { }
 
   ngOnInit() {}
+
+  openFormacionInfo() {
+    this.showFormacionInfo = true;
+    this.renderer2.setStyle(this.pInfo.nativeElement, 'opacity', '1');
+    this.renderer2.setStyle(this.pInfo.nativeElement, 'transition', 'opacity 1s');
+
+  }
+
+  closeFormacionInfo() {
+    setTimeout(() => {
+      this.showFormacionInfo = false;
+    }, 800);
+    this.renderer2.setStyle(this.pInfo.nativeElement, 'opacity', '0');
+    this.renderer2.setStyle(this.pInfo.nativeElement, 'transition', 'opacity 1s');
+  }
 
 }
